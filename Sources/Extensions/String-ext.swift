a741697cc49ff09f5f407ee76aae3a93495a19db
David Spooner <daspoon@me.com>
2022-12-12 18:52:59 -0700

    fixup (improve code generation)

--- Sources/Extensions/String-ext.swift
+++ Sources/Extensions/String-ext.swift
@@ -5,8 +5,17 @@
 
 extension String
   {
+    /// Return the concatenation of a number of newline characters, defaulting to 1.
+    public static func newline(_ n: Int = 1) -> String
+      { .init(repeating: "\n", count: n) }
+
+    /// Return the concatenation of a number of space characters, defaulting to 1.
     public static func space(_ n: Int) -> String
       { .init(repeating: " ", count: n) }
+
+    /// Return a copy of the receiver with each non-initial line prefixed by the given number of space characters.
+    public func indented(_ n: Int) -> String
+      { components(separatedBy: String.newline()).joined(separator: .newline() + .space(n)) }
 
     /// Return an approximate plural form by appending "s"
     var pluralized : String
