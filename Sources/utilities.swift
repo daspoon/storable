c3defe2436f936825d51ed7f5df44bb179d2cc8a
David Spooner <daspoon@me.com>
2022-12-22 10:23:21 -0700

    Move general utility functions from CompendiumUI to Compendium

--- Sources/utilities.swift
+++ Sources/utilities.swift
@@ -2,6 +2,16 @@
 
 */
 
+import CoreData
+
+
+public func log(_ message: @autoclosure () -> String, function: String = #function)
+  { print("--- \(function) - \(message())") }
+
+
+public func sign<T: BinaryInteger>(_ i: T) -> T
+  { i < 0 ? -1 : 1 }
+
 
 func throwingCast<T>(_ v: Any, as: T.Type = T.self) throws -> T
   {
