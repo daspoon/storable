500b1520841f38726e8e856db076aabe961371ed
David Spooner <daspoon@me.com>
2022-12-29 10:31:50 -0700

    Export utility function 'throwingCast'

--- Sources/utilities.swift
+++ Sources/utilities.swift
@@ -13,7 +13,7 @@ public func sign<T: BinaryInteger>(_ i: T) -> T
   { i < 0 ? -1 : 1 }
 
 
-func throwingCast<T>(_ v: Any, as: T.Type = T.self) throws -> T
+public func throwingCast<T>(_ v: Any, as: T.Type = T.self) throws -> T
   {
     guard let t = v as? T else { throw Exception("expecting value of type \(T.self)") }
     return t
