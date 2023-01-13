2da4252d463ef6f270fc12a7e5ae6b229c1fb916
David Spooner <daspoon@me.com>
2023-01-13 14:54:29 -0700

    Move Unpack ingest transform to reside with its use case.

--- Sources/Types/IngestTransform.swift
+++ Sources/Types/IngestTransform.swift
@@ -12,16 +12,3 @@ public protocol IngestTransform
 
     func transform(_ input: Input) throws -> Output
   }
-
-
-
-public struct Unpack : IngestTransform
-  {
-    public init() {}
-
-    public func validate(_ json: Any) throws -> String
-      { try throwingCast(json) }
-
-    public func transform(_ string: String) throws -> [String]
-      { string.map { String($0) } }
-  }
