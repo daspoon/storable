91b109f03c6c5f098ebec7b02fc6db79b5c4eef5
David Spooner <daspoon@me.com>
2023-01-25 09:11:39 -0700

    Restrict supported platforms to enable use of FielManager.temporaryDirectory and URL.appending(component:directoryHint:)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Compendium",
-    platforms: [ .iOS(.v15), .macOS(.v12) ],
+    platforms: [ .iOS(.v16), .macOS(.v13) ],
     products: [
       .library(name: "Compendium", targets: ["Compendium"]),
     ],
