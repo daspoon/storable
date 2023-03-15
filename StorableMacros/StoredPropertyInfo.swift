de5052be4d44dd98739380d73d7b30f4b6849335
David Spooner <daspoon@me.com>
2023-03-15 15:15:45 -0600

    Extend StoredPropertyInfo to maintain the associated attribute/macro (work in progress).

--- StorableMacros/StoredPropertyInfo.swift
+++ StorableMacros/StoredPropertyInfo.swift
@@ -12,12 +12,14 @@ struct StoredPropertyInfo
     var name : String
     var type : TypeSyntax
     var value : ExprSyntax?
-    var attributes : AttributeListSyntax?
+    var attribute : AttributeSyntax
+
 
     init?(_ decl: DeclSyntaxProtocol)
       {
         // Adapted from VariableDeclSyntax.isStoredProperty
 
+        // Ensure the given declaration corresponds to a single variable with a name and a type...
         guard
           let variable = decl.as(VariableDeclSyntax.self),
           let binding = variable.bindings.first, variable.bindings.count == 1,
@@ -27,6 +29,7 @@ struct StoredPropertyInfo
           return nil
         }
 
+        // Ensure the declaration's binding corresponds to a stored property
         switch binding.accessor {
           case .none :
             break
@@ -45,11 +48,40 @@ struct StoredPropertyInfo
             return nil
         }
 
+        // Ensure the declaration has a single attribute among Attribute, Relationship and Fetched...
+        var supportedAttributes : [AttributeSyntax] = []
+        if let attributes = variable.attributes {
+          for element in attributes {
+            guard case .attribute(let attribute) = element else { continue }
+            guard Self.isSupportedAttribute(attribute) else { continue }
+            supportedAttributes.append(attribute)
+          }
+        }
+        switch supportedAttributes.count {
+          case 0 :
+            return nil
+          case 1 :
+            break
+          default :
+            // TODO: emit diagnostic indicating invalidity of multiple attributes...
+            return nil
+        }
+
+        // The declaration has the required format; finish initialization
         self.name = name
         self.type = type
         self.value = binding.initializer?.value
-        self.attributes = variable.attributes
+        self.attribute = supportedAttributes[0]
+      }
+
+
+    static func isSupportedAttribute(_ attribute: AttributeSyntax) -> Bool
+      {
+        switch attribute.attributeName.trimmed.description {
+          case "Attribute", "Relationship" :
+            return true
+          default :
+            return false
+        }
       }
   }
-
-
