8dda6a85d673765cc106a31624a9dcc309d85f50
David Spooner <daspoon@me.com>
2023-04-04 11:18:13 -0600

    Simplify export of base framework imports.

--- Storable/StorableBaseExports.swift
+++ Storable/StorableBaseExports.swift
@@ -2,33 +2,4 @@
 
 */
 
-import CoreData
-import StorableBase
-
-
-public typealias Attribute = StorableBase.Attribute
-public typealias Boxed = StorableBase.Boxed
-public typealias BoxedValueTransformer = StorableBase.BoxedValueTransformer
-public typealias ClassInfo = StorableBase.ClassInfo
-public typealias ClassTree = StorableBase.ClassTree
-public typealias DataStore = StorableBase.DataStore
-public typealias Diffable = StorableBase.Diffable
-public typealias Entity = StorableBase.Entity
-public typealias EntityIngestMethod = StorableBase.EntityIngestMethod
-public typealias Exception = StorableBase.Exception
-public typealias Fetched = StorableBase.Fetched
-public typealias IngestFormat = StorableBase.IngestFormat
-public typealias Ingestible = StorableBase.Ingestible
-public typealias IngestKey = StorableBase.IngestKey
-public typealias IngestMethod = StorableBase.IngestMethod
-public typealias IngestObject = StorableBase.IngestObject
-public typealias IngestSource = StorableBase.IngestSource
-public typealias ManagedObject = StorableBase.ManagedObject
-public typealias Migration = StorableBase.Migration
-public typealias Nullable = StorableBase.Nullable
-public typealias Property = StorableBase.Property
-public typealias Relationship = StorableBase.Relationship
-public typealias Schema = StorableBase.Schema
-public typealias Storable = StorableBase.Storable
-public typealias StorableAsData = StorableBase.StorableAsData
-public typealias StorageType = StorableBase.StorageType
+@_exported import StorableBase
