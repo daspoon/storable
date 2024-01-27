bc4b43d683928169030c7f5093092c9a88015304
David Spooner <daspoon@me.com>
2024-01-27 08:28:36 -0700

    Make Exception internal

--- Sources/Storable/Types/Exception.swift
+++ Sources/Storable/Types/Exception.swift
@@ -9,18 +9,18 @@ import Foundation
 
 /// Exception is the custom Error type thrown by the components of this package.
 
-public struct Exception : LocalizedError
+struct Exception : LocalizedError
   {
-    public let errorDescription : String?
-    public let failureReason : String?
+    let errorDescription : String?
+    let failureReason : String?
 
-    public init(_ reason: String)
+    init(_ reason: String)
       {
         errorDescription = nil
         failureReason = reason
       }
 
-    public init(description d: String, failureReason r: String)
+    init(description d: String, failureReason r: String)
       {
         errorDescription = d
         failureReason = r
@@ -32,12 +32,12 @@ public struct Exception : LocalizedError
 
 extension Exception
   {
-    public static func missingValue(key: String, in context: String?) -> Exception
+    static func missingValue(key: String, in context: String?) -> Exception
       { .init("missing value for '\(key)'" + (context.map({" of \($0)"}) ?? "")) }
 
-    public static func illTypedValue<T>(key: String, type: T.Type, in context: String?) -> Exception
+    static func illTypedValue<T>(key: String, type: T.Type, in context: String?) -> Exception
       { .init("expecting value of type \(T.self) for '\(key)'" + (context.map({" of \($0)"}) ?? "")) }
 
-    public static func illFormedValue(key: String, in context: String?) -> Exception
+    static func illFormedValue(key: String, in context: String?) -> Exception
       { .init("failed to interpret value for '\(key)'" + (context.map({" of \($0)"}) ?? "")) }
   }
